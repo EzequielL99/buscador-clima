c75618d6ebb15906a191b1245fb4b33db5820325
EzequielTecsid <ezequiel@tecsid.com>
2024-10-17 23:29:56 -0300

    Uso de zod y valibot

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,53 +1,93 @@
-import axios from "axios"
-import { SearchType, Weather } from "../types";
+import axios from "axios";
+// import { z } from 'zod';
+import { object, string, number, InferOutput, parse } from "valibot";
+import { SearchType } from "../types";
 
 // TYPE GUARD O ASSERTION
-function isWeatherResponse(weather : unknown) : weather is Weather{
-    return (
-        Boolean(weather) &&
-        typeof weather === 'object' &&
-        typeof (weather as Weather).name === 'string' &&
-        typeof (weather as Weather).main.temp === 'number' &&
-        typeof (weather as Weather).main.temp_max === 'number' &&
-        typeof (weather as Weather).main.temp_min === 'number'
+// function isWeatherResponse(weather : unknown) : weather is Weather{
+//     return (
+//         Boolean(weather) &&
+//         typeof weather === 'object' &&
+//         typeof (weather as Weather).name === 'string' &&
+//         typeof (weather as Weather).main.temp === 'number' &&
+//         typeof (weather as Weather).main.temp_max === 'number' &&
+//         typeof (weather as Weather).main.temp_min === 'number'
 
-    )
-}
+//     )
+// }
+
+// VALIBOT
+const weatherSchema = object({
+  name: string(),
+  main: object({
+    temp: number(),
+    temp_max: number(),
+    temp_min: number(),
+  }),
+});
+
+type Weather = InferOutput<typeof weatherSchema>;
+
+// ZOD
+// const Weather = z.object({
+//     name: z.string(),
+//     main: z.object({
+//         temp: z.number(),
+//         temp_max: z.number(),
+//         temp_min: z.number()
+//     })
+
+// })
+
+// type Weather = z.infer<typeof Weather>;
 
 export default function useWeather() {
-    const fetchWeather = async (search : SearchType) => {
-        const appId = import.meta.env.VITE_API_KEY;
-        try {
-            const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},
+  const fetchWeather = async (search: SearchType) => {
+    const appId = import.meta.env.VITE_API_KEY;
+    try {
+      const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},
             ${search.country}&appid=${appId}`;
 
-            const { data } = await axios(geoURL)
-            const lat = data[0].lat;
-            const lon = data[0].lon;
+      const { data } = await axios(geoURL);
+      const lat = data[0].lat;
+      const lon = data[0].lon;
 
-            const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
-        
-            // Castear el Type
-            // const {data: weatherResult} = await axios<Weather>(weatherURL);
-            // console.log(weatherResult.main.temp);
-            // console.log(weatherResult.name);
+      const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
 
-            // Type Guards
-            const {data: weatherResult} = await axios(weatherURL);
-            const result = isWeatherResponse(weatherResult);
+      // Castear el Type
+      // const {data: weatherResult} = await axios<Weather>(weatherURL);
+      // console.log(weatherResult.main.temp);
+      // console.log(weatherResult.name);
 
-            if(result){
-                console.log(weatherResult.name);
-            }else{
-                console.log('Respuesta equivocada')
-            }
+      // Type Guards
+      // const {data: weatherResult} = await axios(weatherURL);
+      // const result = isWeatherResponse(weatherResult);
 
-        } catch (error) {
-            console.log(error);
-        }
+      // if(result){
+      //     console.log(weatherResult.name);
+      // }else{
+      //     console.log('Respuesta equivocada')
+      // }
+
+      // ZOD
+      //   const { data: weatherResult } = await axios(weatherURL);
+      //   const result = Weather.safeParse(weatherResult);
+      //   if (result.success) {
+      //     console.log(result.data.name);
+      //     console.log(result.data.main.temp);
+      //   }
+
+      // VALIBOT
+      const { data: weatherResult } = await axios(weatherURL);
+      const result = parse(weatherSchema, weatherResult);
+      console.log(result);
+
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return {
-        fetchWeather
-    }
+  return {
+    fetchWeather,
+  };
 }
